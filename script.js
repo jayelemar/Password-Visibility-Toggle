bf67ca74e5faaa5157a3c5cccef9b8c0342d80f9
Jay Termulo <jetermulo@gmail.com>
2023-06-15 11:24:21 +0800

    Initial Mark Up

---
+++ script.js
@@ -0,0 +1,10 @@
+// VARIABLE
+const name = document.querySelector(`CSS-Selector`);
+// EVENT LISTENER
+Node.addEventListener('event-name', callback-func);
+// FUNCTION
+function myFunction(a, b) {
+    return a + b ;
+}
+myFunction(1, 2); /* answer: 3*/
+
